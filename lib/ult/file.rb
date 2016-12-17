f80454439142db9474ff5a3617c7c6c92ac6d085
liveralmask <liveralmask.lisk@gmail.com>
2016-12-18 01:50:46 +0900

    add mkfile

--- lib/ult/file.rb
+++ lib/ult/file.rb
@@ -6,6 +6,12 @@ def filename( path )
   File.basename( path )
 end
 
+def mkfile( path, mode, &block )
+  open( path, mode ){|file|
+    block.call( file )
+  }
+end
+
 def rmfile( path )
   File.delete( path )
 end
