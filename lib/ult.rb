2e1da6a8ffd2a15479f9c15072372b29b7022249
liveralmask <liveralmask.lisk@gmail.com>
2017-01-09 05:40:50 +0900

    add module_function

--- lib/ult.rb
+++ lib/ult.rb
@@ -4,14 +4,38 @@ module Ult
   extend self
   
   require "ult/execute"
+  module_function :execute
+  
+  require "ult/path"
+  module_function :fullpath
+  
   require "ult/option"
+  module_function :option
+  
   require "ult/file"
+  module_function :file?, :filename, :extname, :mkfile, :rmfile
+  
   require "ult/dir"
+  module_function :dir?, :dirname, :direach, :dir, :pwd, :mkdir, :rmdir, :rmkdir
+  
   require "ult/find"
+  module_function :find
+  
   require "ult/platform"
+  module_function :platform, :os_type
+  
   require "ult/replica"
+  module_function :replica
+  
   require "ult/copy"
+  module_function :copy
+  
   require "ult/remove"
+  module_function :remove
+  
   require "ult/watchdog"
+  module_function :watchdog
+  
   require "ult/assert"
+  module_function :assert
 end
